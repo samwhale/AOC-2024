1042d8fd6202563c2bd71238e25141747389508a
Samuel Estrella <samuel@hightouch.io>
2024-12-28 17:10:10 -0800

    Probably not optimal, but it works. day 2

--- src/day2.js
+++ src/day2.js
@@ -4,7 +4,7 @@ const data = readFile('../data/day2.txt')
   .split('\n')
   .map((row) => row.split(' ').map(Number));
 
-function isSafeRow(row) {
+function isSafeRow(row, allowMulligan = false) {
   if (row.length === 0) {
     throw new Error('Row is empty');
   }
@@ -15,12 +15,20 @@ function isSafeRow(row) {
     const difference = row[i] - row[i - 1];
 
     if ((increasing && difference < 0) || (!increasing && difference > 0)) {
+      if (allowMulligan) {
+        return row.some((_, index) => isSafeRow(row.slice(0, index).concat(row.slice(index + 1))));
+      }
+
       return false;
     }
 
     const absoluteDifference = Math.abs(difference);
 
     if (absoluteDifference < 1 || absoluteDifference > 3) {
+      if (allowMulligan) {
+        return row.some((_, index) => isSafeRow(row.slice(0, index).concat(row.slice(index + 1))));
+      }
+
       return false;
     }
   }
@@ -28,4 +36,5 @@ function isSafeRow(row) {
   return true;
 }
 
-console.log('Part 1', data.map(isSafeRow).filter(Boolean).length);
+console.log('Part 1', data.map((row) => isSafeRow(row)).filter(Boolean).length);
+console.log('Part 2', data.map((row) => isSafeRow(row, true)).filter(Boolean).length);
